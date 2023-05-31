432040c5adff4cedebb75fcc39d07bd8744c9e03
josiahx <jnyangaga@multiply.cloud>
2023-05-31 06:41:43 +0400

    Add autograding code to solution

--- test_log_analyzer.py
+++ test_log_analyzer.py
@@ -1,6 +1,8 @@
 import unittest
 from log_analyzer import analyze_logs
 
+from gradescope_utils.autograder_utils.decorators import weight
+
 with open("result_logs/test_results.log") as f:
     LOGS = f.read()
 
@@ -8,6 +10,7 @@ with open("result_logs/test_results_5.log") as f:
     LONG_LOGS = f.read()
 
 class TestLogAnalyzer(unittest.TestCase):
+    @weight(10)
     def test_count(self):
         result = analyze_logs(LOGS)
         count = result.splitlines()[0]
@@ -16,6 +19,7 @@ class TestLogAnalyzer(unittest.TestCase):
         count = result.splitlines()[0]
         self.assertEqual(count, "Number of tests: 1448")
 
+    @weight(10)
     def test_most_used(self):
         result = analyze_logs(LOGS)
         most_used = result.splitlines()[1]
@@ -24,6 +28,7 @@ class TestLogAnalyzer(unittest.TestCase):
         most_used = result.splitlines()[1]
         self.assertEqual(most_used, "Most used type of test: System")
 
+    @weight(10)
     def test_phrase(self):
         result = analyze_logs(LOGS)
         phrase_count = result.splitlines()[2]
@@ -32,6 +37,7 @@ class TestLogAnalyzer(unittest.TestCase):
         phrase_count = result.splitlines()[2]
         self.assertEqual(phrase_count, "Tests related to \"component\": 290")
 
+    @weight(20)
     def test_failures(self):
         result = analyze_logs(LOGS)
         failures = result.splitlines()[4::]
@@ -41,6 +47,7 @@ class TestLogAnalyzer(unittest.TestCase):
         self.assertEqual(failures[3], "Functional  \t2")
         self.assertEqual(failures[4], "System  \t1")
 
+    @weight(10)
     def test_long_log_failures(self):
         result = analyze_logs(LONG_LOGS)
         failures = result.splitlines()[4::]
