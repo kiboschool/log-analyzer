2370256d093d5bda69efdee211e18fc22da6c8db
josiahx <jnyangaga@multiply.cloud>
2023-06-14 17:28:08 +0400

    Add appropriate grade weights

--- test_log_analyzer.py
+++ test_log_analyzer.py
@@ -10,7 +10,7 @@ with open("result_logs/test_results_5.log") as f:
     LONG_LOGS = f.read()
 
 class TestLogAnalyzer(unittest.TestCase):
-    @weight(10)
+    @weight(3)
     def test_count(self):
         result = analyze_logs(LOGS)
         count = result.splitlines()[0]
@@ -19,7 +19,7 @@ class TestLogAnalyzer(unittest.TestCase):
         count = result.splitlines()[0]
         self.assertEqual(count, "Number of tests: 1448")
 
-    @weight(10)
+    @weight(5)
     def test_most_used(self):
         result = analyze_logs(LOGS)
         most_used = result.splitlines()[1]
@@ -28,7 +28,7 @@ class TestLogAnalyzer(unittest.TestCase):
         most_used = result.splitlines()[1]
         self.assertEqual(most_used, "Most used type of test: System")
 
-    @weight(10)
+    @weight(5)
     def test_phrase(self):
         result = analyze_logs(LOGS)
         phrase_count = result.splitlines()[2]
@@ -37,7 +37,7 @@ class TestLogAnalyzer(unittest.TestCase):
         phrase_count = result.splitlines()[2]
         self.assertEqual(phrase_count, "Tests related to \"component\": 290")
 
-    @weight(20)
+    @weight(6)
     def test_failures(self):
         result = analyze_logs(LOGS)
         failures = result.splitlines()[4::]
@@ -47,7 +47,7 @@ class TestLogAnalyzer(unittest.TestCase):
         self.assertEqual(failures[3], "Functional  \t2")
         self.assertEqual(failures[4], "System  \t1")
 
-    @weight(10)
+    @weight(4)
     def test_long_log_failures(self):
         result = analyze_logs(LONG_LOGS)
         failures = result.splitlines()[4::]
@@ -56,6 +56,12 @@ class TestLogAnalyzer(unittest.TestCase):
         self.assertEqual(failures[2], "Performance  \t239")
         self.assertEqual(failures[3], "Functional  \t233")
         self.assertEqual(failures[4], "System  \t233")
+
+    @weight(2)
+    def test_student_submitted_a_running_solution(self):
+        # Check that solution runs without raising any exceptions
+        result = analyze_logs(LOGS)
+
 
 
 if __name__ == "__main__":
