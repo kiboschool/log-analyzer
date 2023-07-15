604e79cf0dc69faaa362119e21411cfe376d9df3
josiahx <jnyangaga@multiply.cloud>
2023-07-15 06:14:31 +0400

    Include some missing files in the final gradescope zip

--- gradescope/make_zip.sh
+++ gradescope/make_zip.sh
@@ -2,8 +2,10 @@
 
 # copy test file
 cp ../test_*.py ./
+cp -r ../result_logs/ ./
 
-zip -r gradescope.zip setup.sh requirements.txt run_autograder run_tests.py test_*.py
+zip -r gradescope.zip setup.sh requirements.txt run_autograder run_tests.py test_*.py result_logs/
 
 # remove test file
 rm ./test_*.py
+rm -rf ./result_logs/
